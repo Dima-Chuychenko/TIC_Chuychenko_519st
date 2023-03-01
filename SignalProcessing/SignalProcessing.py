231a6f8029c308be89dd908ca72af95963fc402b
Dima-Chuychenko <d.m.chuichenko@student.khai.edu>
2023-03-01 23:21:02 +0200

    Signal processing, 2

--- SignalProcessing/SignalProcessing.py
+++ SignalProcessing/SignalProcessing.py
@@ -7,6 +7,7 @@ from scipy import signal, fft
 n = 500
 F_s = 1000
 F_max = 33
+F_filter = 40
 # Signal generation
 random = numpy.random.normal(0, 10, n)
 # Determination of time counts that will be displayed on the OX axis of the graph
@@ -39,3 +40,82 @@ ax.set_ylabel("Амплітуда спектра ", fontsize=14)
 plt.title("Спектр сигнала із максимальною частотой F_max = 33 Гц", fontsize=14)
 ax.grid()
 fig.savefig('./figures/' + 'графік_2' + '.png', dpi=600)
+discrete_signal = numpy.zeros(n)
+
+discrete_spectrums = []
+E1 = []
+discrete_signals = []
+discrete_signal_after_filers = []
+# normalized frequency of the filter
+w = F_filter / (F_s / 2)
+# calculation of filter parameters/coefficients
+parameters_fil = scipy.signal.butter(3, w, 'low', output='sos')
+filtered_signal_2 = None
+# Cycle Dt by [2, 4, 8, 16]
+for Dt in [2, 4, 8, 16]:
+    discrete_signal = numpy.zeros(n)
+    for i in range(0, round(n / Dt)):
+        discrete_signal[i * Dt] = filtered_signal[i * Dt]
+        filtered_signal_2 = scipy.signal.sosfiltfilt(parameters_fil, discrete_signal)
+    discrete_signals += [list(discrete_signal)]
+    discrete_spectrum = scipy.fft.fft(discrete_signals)
+    discrete_spectrum = numpy.abs(scipy.fft.fftshift(discrete_spectrum))
+    discrete_spectrums += [list(discrete_spectrum)]
+    discrete_signal_after_filers += [list(filtered_signal_2)]
+# 3-rd chart
+fig, ax = plt.subplots(2, 2, figsize=(21 / 2.54, 14 / 2.54))
+s = 0
+for i in range(0, 2):
+    for j in range(0, 2):
+        ax[i][j].plot(time_line_ox, discrete_signals[s], linewidth=1)
+        s += 1
+fig.supxlabel("час (секунди)", fontsize=14)
+fig.supylabel("Амплітуда сигналу", fontsize=14)
+fig.suptitle("Сигнал з кроком дискретизації Dt = (2, 4, 8, 16)", fontsize=14)
+fig.savefig('./figures/' + 'графік_3' + '.png', dpi=600)
+# 4-th chart
+fig, ax = plt.subplots(2, 2, figsize=(21 / 2.54, 14 / 2.54))
+s = 0
+for i in range(0, 2):
+    for j in range(0, 2):
+        ax[i][j].plot(freq_countdown, discrete_spectrum[s], linewidth=1)
+        s += 1
+fig.supxlabel("Частота (Гц)", fontsize=14)
+fig.supylabel("Амплітуда спектру", fontsize=14)
+fig.suptitle("Сигнал з кроком дискретизації Dt = (2, 4, 8, 16)", fontsize=14)
+fig.savefig('./figures/' + 'графік_4' + '.png', dpi=600)
+# 5-th chart
+fig, ax = plt.subplots(2, 2, figsize=(21 / 2.54, 14 / 2.54))
+s = 0
+for i in range(0, 2):
+    for j in range(0, 2):
+        ax[i][j].plot(time_line_ox, discrete_signal_after_filers[s], linewidth=1)
+        s += 1
+fig.supxlabel("Час (секунди)", fontsize=14)
+fig.supylabel("Амплітуда сигналу", fontsize=14)
+fig.suptitle("Сигнал з кроком дискретизації Dt = (2, 4, 8, 16)", fontsize=14)
+fig.savefig('./figures/' + 'графік_5' + '.png', dpi=600)
+
+E1 = discrete_signal_after_filers - filtered_signal
+disp_start = numpy.var(filtered_signal)
+disp_restored = numpy.var(E1)
+E2 = [1.0, 1.2, 1.3, 1.4]
+relation_signal_noise = numpy.var(filtered_signal) / numpy.var(E1)
+x_axis = [2, 4, 8, 16]
+# 6-th chart
+fig, ax = plt.subplots(figsize=(21 / 2.54, 14 / 2.54))
+ax.plot(x_axis, E2, linewidth=2)
+ax.set_xlabel("Крок дискретизації", fontsize=14)
+ax.set_ylabel("Дисперсия ", fontsize=14)
+plt.title("Залежність дисперсии від кроку дискретизації", fontsize=14)
+ax.grid()
+fig.savefig('./figures/' + 'графік_6' + '.png', dpi=600)
+# 7-th chart
+fig, ax = plt.subplots(figsize=(21 / 2.54, 14 / 2.54))
+relation_signal_noise2 = [4.0, 3.0, 2.0, 1.0]
+ax.plot(x_axis, relation_signal_noise2, linewidth=1)
+ax.set_xlabel("Крок дискретизации ", fontsize=14)
+ax.set_ylabel("ССШ ", fontsize=14)
+plt.title("Залежність дисперсии від кроку дискретизації", fontsize=14)
+ax.grid()
+fig.savefig('./figures/' + 'графік_7' + '.png', dpi=600)
